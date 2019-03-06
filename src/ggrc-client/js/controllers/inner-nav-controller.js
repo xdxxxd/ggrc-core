c55b8b3e17fedc255bb055da1bf28f29b1aa930e
Roman Matusevich <roma.matusevich@gmail.com>
2019-03-07 00:35:31 +0300

    Change url for project

--- src/ggrc-client/js/controllers/inner-nav-controller.js
+++ src/ggrc-client/js/controllers/inner-nav-controller.js
@@ -156,6 +156,10 @@ export default can.Control.extend({
 
   widget_by_selector: function (selector) {
     return this.options.widget_list.filter((widget) => {
+      if (selector === '#account_balance' && widget.selector === '#project') {
+        return true;
+      }
+
       return widget.selector === selector;
     })[0] || undefined;
   },
