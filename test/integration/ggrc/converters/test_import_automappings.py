102fc73856697c396407c2e19c738bde44de584c
Grandhi Pavan <grandhip@google.com>
2019-08-06 23:38:24 +0530

    Rework test_import_automappings.py

--- test/integration/ggrc/converters/test_import_automappings.py
+++ test/integration/ggrc/converters/test_import_automappings.py
@@ -1,19 +1,53 @@
 # Copyright (C) 2019 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
+""""Test Import Automapping"""
+
+import collections
+
+import ddt
+from ggrc import models
 from integration.ggrc import TestCase
 from integration.ggrc.generator import ObjectGenerator
+from integration.ggrc.models import factories
 
 
+@ddt.ddt
 class TestBasicCsvImport(TestCase):
+  """"Test Import Automapping"""
 
   def setUp(self):
     super(TestBasicCsvImport, self).setUp()
     self.generator = ObjectGenerator()
     self.client.get("/login")
 
-  def test_basic_automappings(self):
-    self.import_file("automappings.csv")
+  @ddt.data("Regulation", "Policy", "Standard", "Contract")
+  def test_basic_automappings(self, object_name):
+    """"Test Basic Automapping for {}"""
+
+    with factories.single_commit():
+      program = factories.ProgramFactory(title="program 1")
+      program_manager = factories.PersonFactory()
+      factories.AccessControlPersonFactory(ac_list=program.
+                                           acr_name_acl_map["Program "
+                                                            "Managers"],
+                                           person=program_manager,)
+      req_one = factories.RequirementFactory(title="section-1")
+
+    self.import_data(collections.OrderedDict([
+        ("object_type", object_name),
+        ("Code*", ""),
+        ("Title", object_name),
+        ("Admin", "user@example.com"),
+        ("map:program", program.slug),
+        ("map:requirement", req_one.slug),
+    ])
+    )
+
+    directive = models.get_model(object_name)
+    directives = directive.query.filter_by(title=object_name).first()
+    directives_slug = directives.slug
+
     data = [{
         "object_name": "Program",
         "filters": {
@@ -26,6 +60,6 @@ class TestBasicCsvImport(TestCase):
         "fields": "all",
     }]
     response = self.export_csv(data)
-    for i in range(1, 8):
-      self.assertIn("reg-{}".format(i), response.data)
-      self.assertIn("section-{}".format(i), response.data)
+
+    self.assertIn(req_one.slug, response.data)
+    self.assertIn(directives_slug, response.data)
