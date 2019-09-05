f7442e78628f766647bd0a9e2044b9f598282ab2
Yury Ostroukh <0str0uchura@gmail.com>
2019-09-05 12:43:14 +0300

    Integrate "Bulk complete" modal with buttons

--- src/ggrc-client/js/components/assessment/assessments-bulk-complete-button/assessments-bulk-complete-button.js
+++ src/ggrc-client/js/components/assessment/assessments-bulk-complete-button/assessments-bulk-complete-button.js
@@ -7,11 +7,34 @@ import canMap from 'can-map';
 import canComponent from 'can-component';
 import canStache from 'can-stache';
 import template from './assessments-bulk-complete-button.stache';
+import {isMyAssessments} from '../../../plugins/utils/current-page-utils';
+
+const viewModel = canMap.extend({
+  isButtonView: false,
+  parentInstance: null,
+  async openBulkCompleteModal(el) {
+    const {AssessmentsBulkComplete} = await import(
+      /* webpackChunkName: "mapper" */
+      '../../../controllers/mapper/mapper'
+    );
+
+    AssessmentsBulkComplete.launch($(el), this.getModalConfig());
+  },
+  getModalConfig() {
+    const parentInstance = this.attr('parentInstance');
+    return {
+      isMyAssessmentsView: isMyAssessments(),
+      mappedToItems: parentInstance ? [{
+        id: parentInstance.attr('id'),
+        type: parentInstance.attr('type'),
+        title: parentInstance.attr('title'),
+      }] : [],
+    };
+  },
+});
 
 export default canComponent.extend({
   tag: 'assessments-bulk-complete-button',
   view: canStache(template),
-  viewModel: canMap.extend({
-    isButtonView: false,
-  }),
+  viewModel,
 });
