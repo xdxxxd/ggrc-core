19acdb6f4dc3eef435d77d3ae47267e3e920628e
Yury Ostroukh <0str0uchura@gmail.com>
2019-09-06 14:09:12 +0300

    Add logic related to select/deselect objects

--- src/ggrc-client/js/components/assessment/assessments-bulk-complete/assessments-bulk-complete.js
+++ src/ggrc-client/js/components/assessment/assessments-bulk-complete/assessments-bulk-complete.js
@@ -9,9 +9,11 @@ import '../../advanced-search/advanced-search-container/advanced-search-containe
 
 import canComponent from 'can-component';
 import canStache from 'can-stache';
+import canBatch from 'can-event/batch/batch';
 import template from './assessments-bulk-complete.stache';
 import ObjectOperationsBaseVM from '../../view-models/object-operations-base-vm';
 import {STATES_KEYS} from '../../../plugins/utils/state-utils';
+import loFindIndex from 'lodash/findIndex';
 
 const viewModel = ObjectOperationsBaseVM.extend({
   showSearch: false,
@@ -22,11 +24,59 @@ const viewModel = ObjectOperationsBaseVM.extend({
   mappingItems: [],
   statesCollectionKey: STATES_KEYS.BULK_COMPLETE,
   type: 'Assessment',
+
+  /**
+   * Contains selected objects (which have id and type properties)
+   */
+  selectedAfterLastSelection: [],
+  deselectedAfterLastSelection: [],
+  /**
+   * Util which removes `newItems`'s items from `collectionForRemove` collection
+   * and add them to `collectionForAdd` collection if they were removed from
+   * `collectionForRemove`.
+   * @param {Array} items - Items for processing
+   * @param {canList} collectionForRemove - Collection in which `items` should be
+   * removed if they are included in `collectionForRemove`
+   * @param {canList} collectionForAdd - Collection which should be filled by the
+   * `items` which weren't added to `collectionForRemove`
+   */
+  updateSelectionCollections(items, collectionForRemove, collectionForAdd) {
+    canBatch.start();
+
+    items.forEach((item) => {
+      const removeItemIndex = loFindIndex(collectionForRemove,
+        (removedItem) => item.attr('id') === removedItem.attr('id'));
+
+      if (removeItemIndex !== -1) {
+        collectionForRemove.splice(removeItemIndex, 1);
+      } else {
+        collectionForAdd.push(item);
+      }
+    });
+
+    canBatch.stop();
+  },
 });
 
 const events = {
   inserted() {
     this.viewModel.onSubmit();
+  },
+  // catch selection of objects by inner components
+  '{viewModel.selected} add'(el, ev, selected) {
+    this.viewModel.updateSelectionCollections(
+      selected,
+      this.viewModel.attr('deselectedAfterLastSelection'),
+      this.viewModel.attr('selectedAfterLastSelection')
+    );
+  },
+  // catch deselection of objects by inner components
+  '{viewModel.selected} remove'(el, ev, deselected) {
+    this.viewModel.updateSelectionCollections(
+      deselected,
+      this.viewModel.attr('selectedAfterLastSelection'),
+      this.viewModel.attr('deselectedAfterLastSelection')
+    );
   },
 };
 
