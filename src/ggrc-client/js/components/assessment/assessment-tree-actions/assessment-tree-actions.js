f6ea892dd91203255ce7d06d3ce984614e650eea
Yury Ostroukh <0str0uchura@gmail.com>
2019-11-04 11:39:46 +0300

    Hide "Bulk Complete" buttons

--- src/ggrc-client/js/components/assessment/assessment-tree-actions/assessment-tree-actions.js
+++ src/ggrc-client/js/components/assessment/assessment-tree-actions/assessment-tree-actions.js
@@ -6,8 +6,6 @@
 import canStache from 'can-stache';
 import canMap from 'can-map';
 import canComponent from 'can-component';
-import {isMyAssessments} from '../../../plugins/utils/current-page-utils';
-import {requestAssessmentsCount} from '../../../plugins/utils/bulk-update-service';
 import template from './assessment-tree-actions.stache';
 
 export default canComponent.extend({
@@ -17,16 +15,6 @@ export default canComponent.extend({
     define: {
       showBulkComplete: {
         value: false,
-        get(lastSetValue, setAttrValue) {
-          setAttrValue(lastSetValue); // set default value before request
-
-          if (isMyAssessments()) {
-            requestAssessmentsCount()
-              .then((count) => {
-                setAttrValue(count > 0);
-              });
-          }
-        },
       },
     },
     instance: null,
