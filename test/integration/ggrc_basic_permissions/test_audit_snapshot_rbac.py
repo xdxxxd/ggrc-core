af01543c9d2fe053a88b6859bd57b603aa523d44
Purnachander Gajjalla <purnachander@google.com>
2019-11-07 16:12:30 +0800

    Refactor test_audit_snapshot_rbac.py

--- test/integration/ggrc_basic_permissions/test_audit_snapshot_rbac.py
+++ test/integration/ggrc_basic_permissions/test_audit_snapshot_rbac.py
@@ -3,21 +3,19 @@
 
 """Test audit RBAC"""
 
-import itertools
-
-from os.path import abspath
-from os.path import dirname
-from os.path import join
 from collections import defaultdict
+import ddt
+
 from integration.ggrc import TestCase
 from integration.ggrc.api_helper import Api
-
 import integration.ggrc.generator
-
+from integration.ggrc.access_control import acl_helper
 from ggrc import db
-from ggrc.models import all_models
+from ggrc.models import all_models, get_model
+from ggrc.snapshotter.rules import Types
 
 
+DEFAULT_PERMISSIONS = defaultdict(lambda: 200)
 DEFAULT_LACK_OF_PERMISSIONS = {
     "Snapshot": 403,
     "Audit": 403
@@ -28,11 +26,10 @@ DEFAULT_AUDITOR_PERMISSIONS = {
 }
 
 
+@ddt.ddt
 class TestAuditRBAC(TestCase):
   """Test audit RBAC"""
   # pylint: disable=too-many-instance-attributes
-
-  CSV_DIR = join(abspath(dirname(__file__)), "test_csvs")
 
   def setUp(self):
     """Imports test_csvs/audit_rbac_snapshot_create.csv needed by the tests"""
@@ -40,78 +37,74 @@ class TestAuditRBAC(TestCase):
     self.api = Api()
     self.objgen = integration.ggrc.generator.ObjectGenerator()
 
-    self.csv_files = itertools.cycle([
-        "audit_rbac_snapshot_create.csv",
-        "audit_rbac_snapshot_update.csv"
-    ])
-
-    self.import_file(next(self.csv_files))
-
-    self.people = all_models.Person.eager_query().all()
-
-    self.auditor_role = all_models.AccessControlRole.query.filter(
-        all_models.AccessControlRole.name == "Auditors"
-    ).one()
-
-    self.program = db.session.query(all_models.Program).filter(
-        all_models.Program.slug == "PMRBACPROGRAM-1"
-    ).one()
-
-    sources = set(r.source for r in self.program.related_sources)
-    destinations = set(r.destination
-                       for r in self.program.related_destinations)
-    related = [obj for obj in sources.union(destinations)
-               if not isinstance(obj, all_models.Person)]
-    self.related_objects = related
-
-    self.api = Api()
-    self.client.get("/login")
-
-    self.audit = self.create_audit()
-
-    self.snapshots = all_models.Snapshot.eager_query().all()
-
-  def create_audit(self):
+  def create_audit(self, audit_role, userid):
     """Create default audit for audit snapshot RBAC tests"""
-    people = {person.email: person for person in self.people}
-    auditor_emails = [
-        "readerauditor@test.com",
-        "creatorauditor@test.com",
-        "editorauditor@test.com",
-        "adminauditor@test.com",
-    ]
+    is_auditor = False
+    if audit_role == "Auditors":
+      is_auditor = True
+      auditor_role = all_models.AccessControlRole.query.filter(
+          all_models.AccessControlRole.name == "Auditors"
+      ).one()
+
+    program = db.session.query(all_models.Program).get(self.program_id)
     _, audit = self.objgen.generate_object(all_models.Audit, {
         "title": "Snapshotable audit",
-        "program": {"id": self.program.id},
+        "program": {"id": self.program_id},
         "status": "Planned",
         "snapshots": {
             "operation": "create",
         },
-        "access_control_list": [{
-            "ac_role_id": self.auditor_role.id,
-            "person": {
-                "id": people[person].id,
-                "type": "Person"
-            }
-        } for person in auditor_emails],
+        "access_control_list": [
+            acl_helper.get_acl_json(auditor_role.id, userid)
+        ] if is_auditor else None,
         "context": {
             "type": "Context",
-            "id": self.program.context_id,
-            "href": "/api/contexts/{}".format(self.program.context_id)
+            "id": program.context_id,
+            "href": "/api/contexts/{}".format(program.context_id)
         }
     })
     return audit
 
+  def create_program(self, program_role, userid):
+    """Create default program for audit snapshot RBAC tests"""
+    if program_role and program_role != "Auditors":
+      program_role = all_models.AccessControlRole.query.filter(
+          all_models.AccessControlRole.name == program_role
+      ).one()
+    else:
+      program_role = None
+
+    _, program = self.objgen.generate_object(all_models.Program, {
+        "title": "test program",
+        "access_control_list": [
+            acl_helper.get_acl_json(program_role.id, userid)
+        ] if program_role else None
+    })
+
+    for model_type in Types.all - Types.external:
+      model = get_model(model_type)
+      _, model_object = self.objgen.generate_object(model, {
+          "title": "Test Snapshot - {}".format(model_type),
+      })
+      self.objgen.generate_relationship(program, model_object)
+
+    return program
+
   def update_audit(self):
     """Update default audit"""
-    self.import_file(next(self.csv_files))
+    for model_type in Types.all - Types.external:
+      model = get_model(model_type)
+      obj = model.query.filter_by(
+          title="Test Snapshot - {}".format(model_type)).first()
+      self.api.modify_object(obj, {
+          "title": "Test Snapshot - {} EDIT".format(model_type)
+      })
 
     audit = all_models.Audit.query.filter(
         all_models.Audit.title == "Snapshotable audit"
     ).one()
-    self.audit = audit
 
-    self.api.modify_object(self.audit, {
+    self.api.modify_object(audit, {
         "snapshots": {
             "operation": "upsert"
         }
@@ -127,7 +120,7 @@ class TestAuditRBAC(TestCase):
 
   def update(self, objects):
     """Attempt to do a PUT request for every object in the objects list"""
-    scope_response = self.api.get(self.audit.__class__, self.audit.id)
+    scope_response = self.api.get(all_models.Audit, self.audit_id)
     if scope_response.status_code == 200:
       self.update_audit()
 
@@ -146,48 +139,106 @@ class TestAuditRBAC(TestCase):
       responses.append((obj.type, status_code))
     return responses
 
-  def call_api(self, method, expected_statuses):
-    """Calls the REST api with a given method and returns a list of
-       status_codes that do not match the expected_statuses dict"""
+  # pylint: disable=attribute-defined-outside-init
+  @ddt.data(
+      ("Administrator", "", DEFAULT_PERMISSIONS),
+      ("Creator", "", DEFAULT_LACK_OF_PERMISSIONS),
+      ("Reader", "", DEFAULT_PERMISSIONS),
+      ("Editor", "", DEFAULT_PERMISSIONS),
+      ("Administrator", "Program Managers", DEFAULT_PERMISSIONS),
+      ("Creator", "Program Managers", DEFAULT_PERMISSIONS),
+      ("Reader", "Program Managers", DEFAULT_PERMISSIONS),
+      ("Editor", "Program Managers", DEFAULT_PERMISSIONS),
+      ("Administrator", "Program Editors", DEFAULT_PERMISSIONS),
+      ("Creator", "Program Editors", DEFAULT_PERMISSIONS),
+      ("Reader", "Program Editors", DEFAULT_PERMISSIONS),
+      ("Editor", "Program Editors", DEFAULT_PERMISSIONS),
+      ("Administrator", "Program Readers", DEFAULT_PERMISSIONS),
+      ("Creator", "Program Readers", DEFAULT_PERMISSIONS),
+      ("Reader", "Program Readers", DEFAULT_PERMISSIONS),
+      ("Editor", "Program Readers", DEFAULT_PERMISSIONS),
+      ("Administrator", "Auditors", DEFAULT_PERMISSIONS),
+      ("Creator", "Auditors", DEFAULT_PERMISSIONS),
+      ("Reader", "Auditors", DEFAULT_PERMISSIONS),
+      ("Editor", "Auditors", DEFAULT_PERMISSIONS),
+  )
+  @ddt.unpack
+  def test_read_access_on_mapped(self, rolename, object_role, expected_status):
+    """Test if {0} with {1} role, has READ access to
+       snapshotted objects of default audit"""
+
+    user = self.create_user_with_role(rolename)
+    user_id = user.id
+
+    program = self.create_program(object_role, user_id)
+    self.program_id = program.id
+
+    audit = self.create_audit(object_role, user_id)
+
+    snapshots = all_models.Snapshot.eager_query().all()
+
+    objects = snapshots + [audit]
+
+    user = all_models.Person.query.get(user_id)
+    self.api.set_user(user)
+
+    responses = self.read(objects)
     all_errors = []
-    for person in self.people:
-      self.api.set_user(person)
-      responses = method(self.snapshots + [self.audit])
-      for type_, code in responses:
-        if code != expected_statuses[person.email][type_]:
-          all_errors.append("{} does not have {} access to {} ({})".format(
-              person.email, method.__name__, type_, code))
-    return all_errors
+    for type_, code in responses:
+      if code != expected_status[type_]:
+        all_errors.append("{} does not have read access to {} ({})".format(
+            user.email, type_, code))
+    assert not all_errors, "\n".join(all_errors)
 
-  def test_read_access_on_mapped(self):
-    """Test READ access to snapshotted objects of default audit"""
-    expected_statuses = defaultdict(lambda: defaultdict(lambda: 200))
-    exceptional_users = (
-        ("creator@test.com", DEFAULT_LACK_OF_PERMISSIONS),
-    )
-    for user, exceptions in exceptional_users:
-      for type_, status_code in exceptions.items():
-        expected_statuses[user][type_] = status_code
-    errors = self.call_api(self.read, expected_statuses)
-    assert not errors, "\n".join(errors)
+  # pylint: disable=attribute-defined-outside-init
+  @ddt.data(
+      ("Administrator", "", DEFAULT_PERMISSIONS),
+      ("Creator", "", DEFAULT_LACK_OF_PERMISSIONS),
+      ("Reader", "", DEFAULT_LACK_OF_PERMISSIONS),
+      ("Editor", "", DEFAULT_PERMISSIONS),
+      ("Administrator", "Program Managers", DEFAULT_PERMISSIONS),
+      ("Creator", "Program Managers", DEFAULT_PERMISSIONS),
+      ("Reader", "Program Managers", DEFAULT_PERMISSIONS),
+      ("Editor", "Program Managers", DEFAULT_PERMISSIONS),
+      ("Administrator", "Program Editors", DEFAULT_PERMISSIONS),
+      ("Creator", "Program Editors", DEFAULT_PERMISSIONS),
+      ("Reader", "Program Editors", DEFAULT_PERMISSIONS),
+      ("Editor", "Program Editors", DEFAULT_PERMISSIONS),
+      ("Administrator", "Program Readers", DEFAULT_PERMISSIONS),
+      ("Creator", "Program Readers", DEFAULT_LACK_OF_PERMISSIONS),
+      ("Reader", "Program Readers", DEFAULT_LACK_OF_PERMISSIONS),
+      ("Editor", "Program Readers", DEFAULT_PERMISSIONS),
+      ("Administrator", "Auditors", DEFAULT_PERMISSIONS),
+      ("Creator", "Auditors", DEFAULT_AUDITOR_PERMISSIONS),
+      ("Reader", "Auditors", DEFAULT_AUDITOR_PERMISSIONS),
+      ("Editor", "Auditors", DEFAULT_PERMISSIONS),
+  )
+  @ddt.unpack
+  def test_update_access_on_mapped(self, rolename,
+                                   object_role, expected_status):
+    """Test if {0} with {1} role, has UPDATE access to
+       snapshotted objects of default audit"""
 
-  def test_update_access_on_mapped(self):
-    """Test UPDATE access to snapshotted objects of default audit"""
-    expected_statuses = defaultdict(lambda: defaultdict(lambda: 200))
+    user = self.create_user_with_role(rolename)
+    user_id = user.id
 
-    exceptional_users = (
-        ("creator@test.com", DEFAULT_LACK_OF_PERMISSIONS),
-        ("reader@test.com", DEFAULT_LACK_OF_PERMISSIONS),
-        ("creatorpr@test.com", DEFAULT_LACK_OF_PERMISSIONS),
-        ("readerpr@test.com", DEFAULT_LACK_OF_PERMISSIONS),
-        # Auditor roles
-        ("readerauditor@test.com", DEFAULT_AUDITOR_PERMISSIONS),
-        ("creatorauditor@test.com", DEFAULT_AUDITOR_PERMISSIONS)
-    )
+    program = self.create_program(object_role, user_id)
+    self.program_id = program.id
 
-    for user, exceptions in exceptional_users:
-      for type_, status_code in exceptions.items():
-        expected_statuses[user][type_] = status_code
+    audit = self.create_audit(object_role, user_id)
+    self.audit_id = audit.id
 
-    errors = self.call_api(self.update, expected_statuses)
-    assert not errors, "\n".join(errors)
+    snapshots = all_models.Snapshot.eager_query().all()
+
+    objects = snapshots + [audit]
+
+    user = all_models.Person.query.get(user_id)
+    self.api.set_user(user)
+
+    responses = self.update(objects)
+    all_errors = []
+    for type_, code in responses:
+      if code != expected_status[type_]:
+        all_errors.append("{} does not have update access to {} ({})".format(
+            user.email, type_, code))
+    assert not all_errors, "\n".join(all_errors)
