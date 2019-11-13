124215ac21a3516d80f5dafa4c380a8a3a76d3f1
YuriChupyrkin <Yuri.Chupyrkin@gmail.com>
2019-11-13 15:07:14 +0300

    Call updateOverflowing when read-more has lazy render

--- src/ggrc-client/js/components/read-more/tests/read-more_spec.js
+++ src/ggrc-client/js/components/read-more/tests/read-more_spec.js
@@ -51,4 +51,121 @@ describe('read-more component', () => {
       }
     });
   });
+
+  describe('init() event', () => {
+    let OriginalObserver;
+    let init;
+    const element = `
+      <div>
+        parent
+        <span> child </span>
+      </div>
+    `;
+
+    const FakeIntersectionObserver = class FakeIntersectionObserver {
+      constructor(callback) {
+        this.callback = callback;
+      }
+      observe() {
+      }
+      unobserve() {
+      }
+    };
+
+    beforeAll(() => {
+      OriginalObserver = window.IntersectionObserver;
+      window.IntersectionObserver = FakeIntersectionObserver;
+    });
+
+    afterAll(() => {
+      window.IntersectionObserver = OriginalObserver;
+    });
+
+    beforeEach(() => {
+      spyOn(vm, 'updateOverflowing');
+
+      init = Component.prototype.init.bind(
+        {
+          viewModel: vm,
+        },
+        element
+      );
+    });
+
+    describe('should call IntersectionObserver.observe when ' +
+    'viewModel.isLazyRender is true', () => {
+      beforeEach(() => {
+        vm.attr('isLazyRender', true);
+      });
+
+      it('with child element', () => {
+        spyOn(IntersectionObserver.prototype, 'observe');
+
+        init();
+
+        expect(IntersectionObserver.prototype.observe)
+          .toHaveBeenCalledWith($(element).children()[0]);
+      });
+
+      it('and callback should call IntersectionObserver.unobserve when ' +
+      'callback\'s arg has "isIntersecting" with "true" value', () => {
+        spyOn(IntersectionObserver.prototype, 'observe')
+          .and.callFake(function () {
+            this.callback([{isIntersecting: true}]);
+          });
+        spyOn(IntersectionObserver.prototype, 'unobserve');
+
+        init();
+        expect(IntersectionObserver.prototype.unobserve).toHaveBeenCalled();
+      });
+
+      it('and callback should call viewModel.updateOverflowing when ' +
+      'callback\'s arg has "isIntersecting" with "true" value', () => {
+        spyOn(IntersectionObserver.prototype, 'observe')
+          .and.callFake(function () {
+            this.callback([{isIntersecting: true}]);
+          });
+        spyOn(IntersectionObserver.prototype, 'unobserve');
+
+        init();
+        expect(vm.updateOverflowing).toHaveBeenCalledWith(element);
+      });
+
+      it('and callback should NOT call IntersectionObserver.unobserve when ' +
+      'callback\'s arg doesn\'t have "isIntersecting" with "true" value',
+      () => {
+        spyOn(IntersectionObserver.prototype, 'observe')
+          .and.callFake(function () {
+            this.callback([{isIntersecting: false}]);
+          });
+        spyOn(IntersectionObserver.prototype, 'unobserve');
+
+        init();
+        expect(IntersectionObserver.prototype.unobserve).not.toHaveBeenCalled();
+      });
+
+      it('and callback should NOT call viewModel.updateOverflowing when ' +
+      'callback\'s arg doesn\'t have "isIntersecting" with "true" value',
+      () => {
+        spyOn(IntersectionObserver.prototype, 'observe')
+          .and.callFake(function () {
+            this.callback([{isIntersecting: false}]);
+          });
+        spyOn(IntersectionObserver.prototype, 'unobserve');
+
+        init();
+        expect(vm.updateOverflowing).not.toHaveBeenCalled();
+      });
+    });
+
+    it('should NOT call IntersectionObserver.observe when ' +
+    'viewModel.isLazyRender is false', () => {
+      vm.attr('isLazyRender', false);
+      spyOn(IntersectionObserver.prototype, 'observe');
+
+      init();
+
+      expect(IntersectionObserver.prototype.observe).not.toHaveBeenCalled();
+    });
+  });
 });
